eed23bb3b1e2bdb0b9690faa48b492905d35c6e1
Ben Fach <benjaminfach@gmail.com>
2020-05-09 10:07:10 -0400

    Completed project

--- redeploy.sh
+++ redeploy.sh
@@ -2,4 +2,4 @@ sudo docker rm manifest-ui -f
 mvn clean package
 mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
 sudo docker build -t secretorganization/shipping-manifest-manager-web-ui .
-sudo docker run -p 8080:8080 -d --name manifest-ui -t secretorganization/shipping-manifest-manager-web-ui
+sudo docker run -p 8080:8080 -d --name manifest-ui --network=rangernet -t secretorganization/shipping-manifest-manager-web-ui
